3544696706f3a4ed87e80be34b84521fe3c6a4c0
choejihwan <cjh87467@gmail.com>
2021-04-06 14:56:42 +0900

    ㅁ add num_04

--- chap_03/src/num_04.java
+++ chap_03/src/num_04.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class num_04 {
+    public static void main(String[] args) {
+
+        Scanner alpa = new Scanner(System.in);
+
+        char input_alpa = alpa.next().charAt(0);
+
+
+        for ( ;   'a' <= input_alpa; input_alpa-- ) {
+            for (char i = 'a'; i <= input_alpa; i++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
 }
