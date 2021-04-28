2fb2f577cdc4e41d675e04c8136e7b098bd5dc50
choejihwan <cjh87467@gmail.com>
2021-04-28 15:44:58 +0900

    MidTest end

---
+++ MyFirstProject/src/test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+
+       int [][] a = new int[5][7];
+
+       for(int i = 0;i<a.length;i++){
+           for(int j = 0; j<a[i].length;j++){
+               System.out.println(a[i][j]);
+           }
+       }
+
+    }
+}
---
+++ chap_04(class)/src/dd.java
@@ -0,0 +1,27 @@
+import  java.util.Scanner;
+public class dd {
+
+    public class a{
+        a (){
+
+        }
+    }
+
+
+
+    public static void main(String [] args) {
+        byte[] b = new byte[2];
+        int[] i = new int[2];
+        float[] f = new float[2];
+        Object[] o = new Object[2];
+        a [] w = new a[2];
+
+
+
+
+        System.out.println(b[1]+","+i[1]+","+f[1]+","+o[1] +","+ w[1]);
+
+    }
+
+
+}
