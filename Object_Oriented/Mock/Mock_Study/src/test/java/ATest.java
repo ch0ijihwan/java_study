35d219b31e911bc24ec46abc474d81de541a7c9a
cjh8746 <cjh87467@gmail.com>
2021-07-14 20:38:58 +0900

    feat : mock을 이해하기 위한 한 study 코드 작성

--- Object_Oriented/Mock/Mock_Study/src/test/java/ATest.java
+++ Object_Oriented/Mock/Mock_Study/src/test/java/ATest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,9 +16,30 @@ class ATest {
     }
 
 
+    @Test
+    @DisplayName("isOlder을 실행했을 때, 객체의 age 보다 크면 true 반환")
+    void isOlder_23() {
+      assertThat(a.isOlder(23)).isTrue();
+    }
+
+    @Test
+    @DisplayName("isOlder을 실행했을 때, 객체의 age 보다 작으면 false 반환")
+    void isOlder_10() {
+        assertThat(a.isOlder(10)).isFalse();
+    }
+
+    @Test
+    @DisplayName("isOlder을 실행했을 때, 객체의 age 보다 작으면 false 반환")
+    void isOlder_3() {
+        assertThat(a.isOlder(3)).isFalse();
+    }
+
+
+
+
     @ParameterizedTest
     @DisplayName("isOlder을 실행했을 때, 객체의 age 보다 큰 나이가 들어가면 true 반환, 그렇지 않으면 false 반환 ")
-    @CsvSource(value = {"15,true", "1,false", "10,false"})
+    @CsvSource(value = {"23,true", "10,false", "3,false"})
     void isOrder(int age, boolean expect) {
         assertThat(a.isOlder(age)).isEqualTo(expect);
     }
